accdb9267dac8b6c6c8a5d2b733c234c928fe2c4
Bruce Wayne <admin@bige0.com>
2020-11-30 10:15:21 +0800

    Reformat

--- ReactiveDemo/App.xaml.cs
+++ ReactiveDemo/App.xaml.cs
@@ -1,17 +1,17 @@
-﻿using ReactiveUI;
+using ReactiveUI;
 using Splat;
 using System.Reflection;
 
 namespace ReactiveDemo
 {
-    public partial class App
-    {
-        public App()
-        {
-            // A helper method that will register all classes that derive off IViewFor 
-            // into our dependency injection container. ReactiveUI uses Splat for it's 
-            // dependency injection by default, but you can override this if you like.
-            Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
-        }
-    }
+	public partial class App
+	{
+		public App()
+		{
+			// A helper method that will register all classes that derive off IViewFor 
+			// into our dependency injection container. ReactiveUI uses Splat for it's 
+			// dependency injection by default, but you can override this if you like.
+			Locator.CurrentMutable.RegisterViewsForViewModels(Assembly.GetCallingAssembly());
+		}
+	}
 }
--- ReactiveDemo/ViewModels/NugetDetailsViewModel.cs
+++ ReactiveDemo/ViewModels/NugetDetailsViewModel.cs
@@ -1,4 +1,4 @@
-﻿using NuGet.Protocol.Core.Types;
+using NuGet.Protocol.Core.Types;
 using ReactiveUI;
 using System;
 using System.Diagnostics;
@@ -7,35 +7,35 @@ using System.Reactive.Linq;
 
 namespace ReactiveDemo.ViewModels
 {
-    // This class wraps out NuGet model object into a ViewModel and allows
-    // us to have a ReactiveCommand to open the NuGet package URL.
-    public class NugetDetailsViewModel : ReactiveObject
-    {
-        private readonly IPackageSearchMetadata _metadata;
-        private readonly Uri _defaultUrl;
+	// This class wraps out NuGet model object into a ViewModel and allows
+	// us to have a ReactiveCommand to open the NuGet package URL.
+	public class NugetDetailsViewModel : ReactiveObject
+	{
+		private readonly IPackageSearchMetadata _metadata;
+		private readonly Uri _defaultUrl;
 
-        public NugetDetailsViewModel(IPackageSearchMetadata metadata)
-        {
-            _metadata = metadata;
-            _defaultUrl = new Uri("https://git.io/fAlfh");
-            OpenPage = ReactiveCommand.CreateFromObservable(() =>
-            {
-                return Observable.Start(() =>
-                {
-                    Process.Start(new ProcessStartInfo(ProjectUrl.ToString()) { UseShellExecute = true });
-                });
-            });
-        }
+		public NugetDetailsViewModel(IPackageSearchMetadata metadata)
+		{
+			_metadata = metadata;
+			_defaultUrl = new Uri("https://git.io/fAlfh");
+			OpenPage = ReactiveCommand.CreateFromObservable(() =>
+			{
+				return Observable.Start(() =>
+				{
+					Process.Start(new ProcessStartInfo(ProjectUrl.ToString()) { UseShellExecute = true });
+				});
+			});
+		}
 
-        public Uri IconUrl => _metadata.IconUrl ?? _defaultUrl;
-        public string Description => _metadata.Description;
-        public Uri ProjectUrl => _metadata.ProjectUrl;
-        public string Title => _metadata.Title;
+		public Uri IconUrl => _metadata.IconUrl ?? _defaultUrl;
+		public string Description => _metadata.Description;
+		public Uri ProjectUrl => _metadata.ProjectUrl;
+		public string Title => _metadata.Title;
 
-        // ReactiveCommand allows us to execute logic without exposing any of the 
-        // implementation details with the View. The generic parameters are the 
-        // input into the command and it's output. In our case we don't have any 
-        // input or output so we use Unit which in Reactive speak means a void type.
-        public ReactiveCommand<Unit, Unit> OpenPage { get; }
-    }
+		// ReactiveCommand allows us to execute logic without exposing any of the 
+		// implementation details with the View. The generic parameters are the 
+		// input into the command and it's output. In our case we don't have any 
+		// input or output so we use Unit which in Reactive speak means a void type.
+		public ReactiveCommand<Unit, Unit> OpenPage { get; }
+	}
 }
